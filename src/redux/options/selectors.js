278649c8eaf8863579acac9b3d1266456d51a490
Natalka-01 <nataliia.dubikova@gmail.com>
2026-08-22 14:52:34 +0200

    Add options slice and selectors for categories, areas, and ingredients

---
+++ src/redux/options/selectors.js
@@ -0,0 +1,4 @@
+export const selectCategories = (state) => state.options.categories;
+export const selectAreas = (state) => state.options.areas;
+export const selectIngredients = (state) => state.options.ingredients;
+export const selectOptionsIsLoading = (state) => state.options.isLoading;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { authReducer } from "./auth/slice";
+import { optionsReducer } from "./options/slice";
 
 import {
   persistStore,
@@ -25,6 +26,7 @@ const authPersistConfig = {
 export const store = configureStore({
   reducer: {
     auth: persistReducer(authPersistConfig, authReducer),
+    options: optionsReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
