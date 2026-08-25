3feb34fc70565373c158b92e829a1165bfd6c673
Vitalii Vasylets <mohara88s@gmail.com>
2026-08-25 16:49:25 +0300

    fix heart icon

--- src/components/RecipeCard/RecipeCard.jsx
+++ src/components/RecipeCard/RecipeCard.jsx
@@ -77,8 +77,7 @@ export default function RecipeCard({
 							}
 						>
 							<svg className={style.icon} aria-hidden="true">
-								{/* <use href="/icons.svg#heart" /> */}
-								<use href={`${import.meta.env.BASE_URL}icons.svg#heart`} />
+								<use href="/icons.svg#heart" />
 							</svg>
 						</button>
 
