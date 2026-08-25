1ad485f6884fae530ecbaa0928a8aa7b6f646089
Vitalii Vasylets <mohara88s@gmail.com>
2026-08-25 16:06:22 +0300

    fix vite base

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-export default defineConfig(({ command }) => {
-  return {
-    base: command === 'build' ? '/recipe-service-app/' : '/',
-
-    plugins: [react()],
-    build: {
-      sourcemap: true,
-    },
-  };
+export default defineConfig({
+  base: '/',
+  plugins: [react()],
+  build: {
+    sourcemap: true,
+  },
 });
