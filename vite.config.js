04661b757f4c1fc85f86d4a009654aedbf2c2c58
Vitalii Vasylets <mohara88s@gmail.com>
2026-08-25 15:37:39 +0300

    fix no base at vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-export default defineConfig(({ command }) => {
-  return {
-    base: command === 'build' ? '/recipe-service-app/' : '/',
-
-    plugins: [react()],
-    build: {
-      sourcemap: true,
-    },
-  };
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    sourcemap: true,
+  },
 });
