ff8e98c83e3146bff2603b1efc9e741d3ebbe8e6
Mohara88s <73835861+Mohara88s@users.noreply.github.com>
2026-08-27 19:30:46 +0000

    Deploying to gh-pages from @ project-node-react/recipe-service-app@5ac60661ee9183f8f39c98bc26587dca0ca60577 🚀

---
+++ assets/MainTitle-C-i4znIB.js
@@ -0,0 +1,2 @@
+import{N as e}from"./index-CH5RGHWX.js";var t={title:`_title_zr1lp_1`},n=e(),r=({children:e})=>(0,n.jsx)(`h1`,{className:t.title,children:e});export{r as t};
+//# sourceMappingURL=MainTitle-C-i4znIB.js.map
