ebae309a2c6844f0d0472d2993589343f457daef
Mohara88s <73835861+Mohara88s@users.noreply.github.com>
2026-08-27 18:42:38 +0000

    Deploying to gh-pages from @ project-node-react/recipe-service-app@598d95851c4569a4961dcb131a7d8cd07adf5b1d 🚀

---
+++ assets/MainTitle-gaw-rRhl.js
@@ -0,0 +1,2 @@
+import{N as e}from"./index-CuXIUjEB.js";var t={title:`_title_tvu97_1`},n=e(),r=({children:e})=>(0,n.jsx)(`h1`,{className:t.title,children:e});export{r as t};
+//# sourceMappingURL=MainTitle-gaw-rRhl.js.map
