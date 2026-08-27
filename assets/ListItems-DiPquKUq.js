906c7f10e90a54d18ab63fafa6a0601189ac69a0
Mohara88s <73835861+Mohara88s@users.noreply.github.com>
2026-08-27 08:41:08 +0000

    Deploying to gh-pages from @ project-node-react/recipe-service-app@d38c0594b6ac157fc716125e4a2ffa11377ab9a0 🚀

--- assets/ListItems-DiPquKUq.js
+++
@@ -1,2 +0,0 @@
-import{d as e,f as t,l as n,m as r,t as i}from"./jsx-runtime-BIT6nIgj.js";import{k as a}from"./index-C-whV_HR.js";r();var o=i();function s(){let{id:r}=e(),i=n();a();let[s,c]=t();Number(s.get(`page`));let l=i.pathname.split(`/`).filter(Boolean),u=l[l.length-1]||`my-recipes`;return console.log(u),(0,o.jsx)(`div`,{})}export{s as default};
-//# sourceMappingURL=ListItems-DiPquKUq.js.map
