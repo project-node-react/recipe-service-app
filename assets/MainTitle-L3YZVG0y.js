d0f715a77d5765a447d2c0c8fbbf1a929715876b
Mohara88s <73835861+Mohara88s@users.noreply.github.com>
2026-08-28 16:46:42 +0000

    Deploying to gh-pages from @ project-node-react/recipe-service-app@2b355e0d392bed79bdec4f207d931de5a35653a4 🚀

---
+++ assets/MainTitle-L3YZVG0y.js
@@ -0,0 +1,2 @@
+import{N as e}from"./index-YGTPTD7U.js";var t={title:`_title_zr1lp_1`},n=e(),r=({children:e})=>(0,n.jsx)(`h1`,{className:t.title,children:e});export{r as t};
+//# sourceMappingURL=MainTitle-L3YZVG0y.js.map
---
+++ assets/Subtitle-PkRJ_OT1.js
@@ -0,0 +1,2 @@
+import{N as e}from"./index-YGTPTD7U.js";var t={subtitle:`_subtitle_7xww0_1`},n=e(),r=({children:e})=>(0,n.jsx)(`h2`,{className:t.subtitle,children:e});export{r as t};
+//# sourceMappingURL=Subtitle-PkRJ_OT1.js.map
