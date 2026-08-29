7901a3d7f95797e0d675b874b9bca782e5ca2b0b
Mohara88s <73835861+Mohara88s@users.noreply.github.com>
2026-08-29 18:00:33 +0000

    Deploying to gh-pages from @ project-node-react/recipe-service-app@e34af5c8ede2a751d74f2a7ea14ef4229881ad60 🚀

--- assets/Subtitle-DTBVuTaG.js
+++
@@ -1,2 +0,0 @@
-import{F as e}from"./index-pqqJIDi4.js";var t={subtitle:`_subtitle_vqrm1_1`},n=e(),r=({children:e})=>(0,n.jsx)(`h2`,{className:t.subtitle,children:e});export{r as t};
-//# sourceMappingURL=Subtitle-DTBVuTaG.js.map
