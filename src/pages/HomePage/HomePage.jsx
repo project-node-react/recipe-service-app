64c4bf3aa78b3adbd89bf3459d910f37835ff9da
Vitalii <mohara88s@gmail.com>
2026-08-29 20:58:53 +0300

    fix container at homePage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,14 +1,13 @@
 import { useCallback, useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
-import Container from "../../components/Container/Container";
 import CategoryList from "../../components/CategoryList/CategoryList";
 import SignInModal from "../../components/SignInModal/SignInModal";
 
 import { fetchRecipes } from "../../redux/recipes/operations";
 import {
-	selectRecipesPage,
-	selectRecipesFilters,
+  selectRecipesPage,
+  selectRecipesFilters,
 } from "../../redux/recipes/selectors";
 
 import SignUpModal from "../../components/SignUpModal/SignUpModal";
@@ -18,45 +17,45 @@ import Testimonials from "../../components/Testimonials/Testimonials";
 import RecipesComponent from "../../components/RecipesComponent/RecipesComponent";
 
 export default function HomePage() {
-	const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-	const page = useSelector(selectRecipesPage);
-	const filters = useSelector(selectRecipesFilters);
+  const page = useSelector(selectRecipesPage);
+  const filters = useSelector(selectRecipesFilters);
 
-	const [isSignInOpen, setIsSignInOpen] = useState(false);
-	const [isSignUpOpen, setIsSignUpOpen] = useState(false);
+  const [isSignInOpen, setIsSignInOpen] = useState(false);
+  const [isSignUpOpen, setIsSignUpOpen] = useState(false);
 
-	// Кожна зміна сторінки або фільтрів — це новий запит на бекенд:
-	// пагінація серверна, а не нарізка вже завантаженого масиву.
-	useEffect(() => {
-		dispatch(fetchRecipes());
-	}, [dispatch, page, filters]);
+  // Кожна зміна сторінки або фільтрів — це новий запит на бекенд:
+  // пагінація серверна, а не нарізка вже завантаженого масиву.
+  useEffect(() => {
+    dispatch(fetchRecipes());
+  }, [dispatch, page, filters]);
 
-	const handleAuthRequired = useCallback(() => setIsSignInOpen(true), []);
-	const isCategorySelected = Boolean(filters?.category);
-	return (
-		<Container>
-			<Hero onAuthRequired={handleAuthRequired} />
-			{isCategorySelected ? <RecipesComponent /> : <CategoryList />}
+  const handleAuthRequired = useCallback(() => setIsSignInOpen(true), []);
+  const isCategorySelected = Boolean(filters?.category);
+  return (
+    <>
+      <Hero onAuthRequired={handleAuthRequired} />
+      {isCategorySelected ? <RecipesComponent /> : <CategoryList />}
 
-			<Testimonials />
+      <Testimonials />
 
-			<SignInModal
-				isOpen={isSignInOpen}
-				onClose={() => setIsSignInOpen(false)}
-				onCreateAccount={() => {
-					setIsSignInOpen(false);
-					setIsSignUpOpen(true);
-				}}
-			/>
-			<SignUpModal
-				isOpen={isSignUpOpen}
-				onClose={() => setIsSignUpOpen(false)}
-				onSignInAccount={() => {
-					setIsSignInOpen(true);
-					setIsSignUpOpen(false);
-				}}
-			/>
-		</Container>
-	);
+      <SignInModal
+        isOpen={isSignInOpen}
+        onClose={() => setIsSignInOpen(false)}
+        onCreateAccount={() => {
+          setIsSignInOpen(false);
+          setIsSignUpOpen(true);
+        }}
+      />
+      <SignUpModal
+        isOpen={isSignUpOpen}
+        onClose={() => setIsSignUpOpen(false)}
+        onSignInAccount={() => {
+          setIsSignInOpen(true);
+          setIsSignUpOpen(false);
+        }}
+      />
+    </>
+  );
 }
